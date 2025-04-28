ce15aaaffc0671f6e2c0fce718ad18f46c9d3a3e
siblinggrief <sibling.grief.processing@gmail.com>
2025-04-28 22:22:08 +0530

    Delete post from Tag Page

--- client/src/pages/TagPage.js
+++ client/src/pages/TagPage.js
@@ -1,15 +1,23 @@
-// src/pages/TagPage.js
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Typography, Box } from "@mui/material";
-import Post from "../components/Post"; // Reuse your Post component!
+import { Typography, Box, CircularProgress } from "@mui/material";
+import Post from "../components/Post";
 import { usePosts } from '../context/PostsContext';
 
 const TagPage = () => {
   const { tagName } = useParams();
-  const { posts } = usePosts();
+  const { posts, deletePost, loading } = usePosts();
+  
+  const [filteredPosts, setFilteredPosts] = useState([]);
 
-  const filteredPosts = posts.filter(post => post.topics?.includes(tagName));
+  useEffect(() => {
+    const filtered = posts.filter(post => post.topics?.includes(tagName));
+    setFilteredPosts(filtered);
+  }, [posts, tagName]);
+
+  const handlePostDeleted = (postId) => {
+    deletePost(postId);
+  };
 
   return (
     <Box>
@@ -17,13 +25,17 @@ const TagPage = () => {
         Posts tagged with: {tagName}
       </Typography>
 
-      {filteredPosts.length > 0 ? (
+      {loading ? (
+        <Box display="flex" justifyContent="center" mt={3}>
+          <CircularProgress />
+        </Box>
+      ) : filteredPosts.length > 0 ? (
         filteredPosts.map((post) => (
-          <Post key={post.id} post={post} />
+          <Post key={post.id} post={post} onPostDeleted={handlePostDeleted} />
         ))
       ) : (
-        <Typography variant="body2" mt={3}>
-          No posts found for {tagName}.
+        <Typography variant="body2" textAlign="center" mt={4}>
+          No posts found for this topic: {tagName}. Be the first to share!
         </Typography>
       )}
     </Box>
